f6f7d4d406a22a26a2e328ca8e54e8841f18acc5
frostep <frostell.matters@gmail.com>
2023-03-03 07:05:25 +0300

    add dinamic apearing box

--- frontend/public/index.js
+++ frontend/public/index.js
@@ -14,6 +14,7 @@ function startGame() {
 
 function handleBoxClick(event) {
   if (event.target.dataset) {
+    console.log(event.target.dataset);
     score++;
     renderBox();
   }
@@ -24,15 +25,24 @@ function renderBox() {
   // создаем тег div
   let box = document.createElement('div');
 
+  let boxSize = getRandom(30, 100);
+  let gameSize = $game.getBoundingClientRect();
+  let maxTop = gameSize.height - boxSize;
+  let maxLeft = gameSize.width - boxSize;
+
   // задаем параметры css блоку box
-  box.style.height = box.style.width = '50px';
+  box.style.height = box.style.width = `${boxSize}px`;
   box.style.position = 'absolute';
   box.style.backgroundColor = '#000';
-  box.style.top = '50px';
-  box.style.left = '70px';
+  box.style.top = `${getRandom(0, maxTop)}px`;
+  box.style.left = `${getRandom(0, maxLeft)}px`;
   box.style.cursor = 'pointer';
-  box.setAttribute('data-box', 'true');
+  box.setAttribute('data-box', true);
 
   //вставляем в блок game наш тег box
   $game.insertAdjacentElement('afterbegin', box);
 }
+
+const getRandom = (min, max) => {
+  return Math.floor(Math.random() * (max - min) + min);
+};
